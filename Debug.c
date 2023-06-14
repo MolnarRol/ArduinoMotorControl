e250718bb6d8938af4295a66c03b8c6f9999f3cb
Roland Molnar <molnarroland99@gmail.com>
2023-06-14 14:23:24 +0200

    Typo fix

--- Debug.c
+++ Debug.c
@@ -5,7 +5,7 @@ inline void setPin()
   PORTD |= 0b00000100;
 };
 
-inline void clearPin();
+inline void clearPin()
 {
   PORTD &= 0b11111011;
 };
--- Debug.h
+++ Debug.h
@@ -1,9 +1,13 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
+#include "Arduino.h"
 
 #ifdef __cplusplus
 extern "C"{
 #endif
+/*
+  Using Arduino pin D2 as debug pin
+*/
 inline void setPin();
 inline void clearPin();
 #ifdef __cplusplus
