c5d978345a7faa49c4d7e04e3af610876bb20d6c
Molnar <molnarroland99@gmail.com>
2023-07-24 20:25:42 +0200

    main cleanup

--- ArduinoMotorControl/inc/TimingUtils.h
+++ ArduinoMotorControl/inc/TimingUtils.h
@@ -4,22 +4,13 @@
 #include "Arduino.h"
 #include "../config.h"
 
-<<<<<<< HEAD
 typedef struct pulseStruct {
   volatile uint32_t pulses[PULSES_MAX];
-=======
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef struct pulseStruct {
-  uint32_t pulses[PULSES_MAX];
->>>>>>> origin/main
   volatile uint8_t pulseIdx;
 } pulseTypeDef;
 
 typedef struct pulseBuffers {
-<<<<<<< HEAD
+
   volatile pulseTypeDef buffer[2];
   volatile uint8_t idx;
 } pulseBuffersTypeDef;
@@ -34,23 +25,6 @@ uint32_t calcTimCntAVG( uint32_t* values, uint8_t len );
 float filter_1stOrder_r32(float r32ActualValue, float r32SmoothedValue, float r32SamplingTime_ms, float r32FilterTime_ms);
 float getRPMfromPulses( void );
 float calcRPM( const uint32_t cnt );
-
-=======
-  pulseTypeDef buffer[2];
-  uint8_t idx;
-} pulseBuffersTypeDef;
-
-<<<<<<< HEAD
-void switchPulseBuff();
-void writePulseBuff( const uint32_t val );
-=======
-void writePulseBuff( uint32_t val );
->>>>>>> parent of f48da27... Boost start - bugfix
-uint32_t calcTimCntAVG( uint32_t* values, uint8_t len );
-float filter_1stOrder_r32(float r32ActualValue, float r32SmoothedValue, float r32SamplingTime_ms, float r32FilterTime_ms);
-float getRPMfromPulses();
-float calcRPM( const uint32_t cnt );
-
 #ifdef __cplusplus
 }
 #endif
