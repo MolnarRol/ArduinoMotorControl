dc9fdab792a931717cb2e24f92d764b29f25c031
Molnar <molnarroland99@gmail.com>
2023-07-24 20:23:32 +0200

    cleanup

--- ArduinoMotorControl/inc/TimingUtils.h
+++ ArduinoMotorControl/inc/TimingUtils.h
@@ -24,5 +24,4 @@ uint32_t calcTimCntAVG( uint32_t* values, uint8_t len );
 float filter_1stOrder_r32(float r32ActualValue, float r32SmoothedValue, float r32SamplingTime_ms, float r32FilterTime_ms);
 float getRPMfromPulses( void );
 float calcRPM( const uint32_t cnt );
-
 #endif
