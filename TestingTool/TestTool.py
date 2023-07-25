5f1365f411064fae7439f9ceb081f19325f42a49
Roland Molnar <roland.molnar@nmb-minebea.com>
2023-07-25 15:12:09 +0200

    SourceTree testcommit

--- TestingTool/TestTool.py
+++ TestingTool/TestTool.py
@@ -8,6 +8,9 @@ WAIT_KEYWORD_LEN = len(WAIT_KEYWORD)
 
 file = open( "test.txt", "r" ).read()
 commandList = file.split('\n')
+COM_PORT = commandList[0]
+print(COM_PORT)
+
 for item in commandList:
     if item[ 0:WAIT_KEYWORD_LEN ] == WAIT_KEYWORD:
         sleep( float( item[(WAIT_KEYWORD_LEN + 1):] ) )
